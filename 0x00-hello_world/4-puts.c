0c5a68379f5a3383cd820233b73cdf8bc42d43ed
Programlucy <programtesting577@gmail.com>
2022-12-08 10:34:56 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * Include main with a print output programming is like building
+ * Return: 0 output
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
