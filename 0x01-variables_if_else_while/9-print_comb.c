b2f6090aa2ad9f5750df06f17f92285cf9a85c5a
Programlucy <programtesting577@gmail.com>
2022-12-09 13:01:34 -0800

    The last tasks in C Variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,12 +3,11 @@
 
 
 /**
+ * main - prints all possible combinations of single-digit numbers
  *
- *  * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  *
- *   * Return: Always 0 (Success)
- *
- *    */
+ */
 
 int main(void)
 
