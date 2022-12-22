013fd72d8ed71a99b54f8c731fb24d35c9f7d99e
Programlucy <programtesting577@gmail.com>
2022-12-22 05:23:36 -0800

    Corrections

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-iint _putchar(char c);
-
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -10,10 +8,8 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *);
+char *string_toupper(char *s);
 
-char *cap_string(char *);
+char *cap_string(char *s);
 
-char *leet(char *);
-
-char *rot13(char *);
+char *leet(char *s);
